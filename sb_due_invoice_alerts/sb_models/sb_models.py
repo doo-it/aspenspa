f1bf8c02e8fc1e0981e973f494b3c07d31dab54a
snippetbucket <snippetbucket@gmail.com>
2019-03-07 20:20:22 +0530

    re-updated snippetbucket.com

--- sb_due_invoice_alerts/sb_models/sb_models.py
+++ sb_due_invoice_alerts/sb_models/sb_models.py
@@ -9,6 +9,12 @@ import datetime
 from datetime import *
 
 
+# class calendar_event(models.Model):
+#     _inherit="calendar.event"
+
+#     stop = fields.Datetime('Stop', required=False, help="Stop date of an event, without time for full days events")
+
+
 class account_invoice(models.Model):
     _inherit="account.invoice"
 
@@ -36,17 +42,51 @@ class account_invoice(models.Model):
             date_format = self.env['res.lang'].sudo().search([('active','=',True)],limit=1).date_format
             name = "Due Invoice: %s on %s"%(self.number,datetime.strptime(str(self.date_due), '%Y-%m-%d').strftime(date_format))
             entry_date = str(self.date_due) + " 04:30:00"
+            # stop_date = str(self.date_due) + " 04:29:59"
+            partner_id = self.user_id.partner_id.id
             if self.user_id:
                 invitee_ids.append(self.user_id.partner_id.id)
-            new_event_obj = _cal.sudo().create({
-                        'name':name,
-                        'stop':entry_date,
-                        'start':entry_date,
-                        'start_datetime':entry_date,
-                        'alarm_ids':[(4,x) for x in alarm_ids],
-                        'privacy':'confidential',
-                        'show_as':'free',
-                })
+            vals = {
+                    'partner_ids': [[6, False, [partner_id]]], 
+                    'allday': False, 
+                    'interval': 1, 
+                    'end_type': 'count', 
+                    'count': 1, 
+                    'month_by': 'date', 
+                    'day': 1, 
+                    'privacy': 'confidential', 
+                    'show_as': 'free', 
+                    'user_id': 2, 
+                    'res_id': 0, 
+                    'name': name, 
+                    'start': entry_date, 
+                    'stop': entry_date, 
+                    'start_date': False, 
+                    'stop_date': False, 
+                    'start_datetime': entry_date, 
+                    'stop_datetime': entry_date, 
+                    'duration': 24.0, 
+                    'categ_ids': [[6, False, []]], 
+                    'alarm_ids': [(4,x) for x in alarm_ids], 
+                    'location': False, 
+                    'description': False, 
+                    'recurrency': False, 
+                    'rrule_type': False, 
+                    'final_date': False, 
+                    'mo': False, 
+                    'tu': False, 
+                    'we': False, 
+                    'th': False, 
+                    'fr': False, 
+                    'sa': False, 
+                    'su': False, 
+                    'byday': False, 
+                    'week_list': False, 
+                    'recurrent_id': 0, 
+                    'message_attachment_count': 0
+                }
+            new_event_obj = _cal.sudo().create(vals)
+            self.env.cr.commit()
             if new_event_obj: 
                 for i in self.message_follower_ids:
                     new_attendee = _att.sudo().create({
@@ -55,7 +95,8 @@ class account_invoice(models.Model):
                             'email':i.partner_id.email,
                             'event_id':new_event_obj.id,
                         })
-                new_event_obj.duration = 8.5
+                new_event_obj.duration = 24.0
+                # new_event_obj.stop = entry_date
                 self.calendar_event_alert_id = new_event_obj.id
         return obj
 
